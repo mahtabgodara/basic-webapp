bafc3adf85e22c89541909b94c446a93244ade83
mahtab.singh <mahtab.singh@thalesgroup.com>
2022-03-24 01:25:54 +0530

    overhaul

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from waitress import serve
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello world from Python\n"
+    return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
-
-
+    serve(app, host='0.0.0.0', port=8080)
